d414773e51df9852beccc6dc4111ce7c66cc9993
Liam Wilson <lwilson83@csu.fullerton.edu>
2026-06-23 16:50:41 -1000

    Added functions for all set up phase actions

--- src/player.py
+++ src/player.py
@@ -1,47 +1,116 @@
 import random
+from card import Card, Attack, Ability, PokemonCard, TrainerCard, EnergyCard
+from globals import EFFECTS_DICT, ACTION_DICT
 
 class Player:
-    def __init__(self, deck):
-        self.deck = deck
-        self.hand = []
-        self.active = None
-        self.bench = []
-        self.prizes = []
-        self.time = 0.0
+    def __init__(self, deck: list[Card]):
+        self.deck: list[Card] = deck
+        self.hand: list[Card] = []
+        self.active: PokemonCard | None = None
+        self.bench: list[PokemonCard] = []
+        self.prizes: list[Card] = []
+        self.discard_pile: list[Card] = []
+        self.time: float = 0.0
 
         # One use per turn actions
-        self.energy_attached = False
-        self.supporter_played = False
-        self.retreated = False
+        self.energy_attached: bool = False
+        self.supporter_played: bool = False
+        self.retreated: bool = False
 
+    # Set up phase
     def shuffle_deck(self):
         '''
         '''
-        count = random.randint(1, 5)
-        for _ in range(count):
+        for _ in range(random.randint(1, 5)):
             random.shuffle(self.deck)
 
-    def draw(self, amount = 1):
+        return True
+    
+    def draw_hand(self):
         '''
         '''
-        for _ in range(amount):
-            if self.deck:
-                card = self.deck.pop(0)
-                self.hand.append(card)
+        for _ in range(7):
+            card = self.deck.pop(0)
+            self.hand.append(card)
+
+        return True
+    
+    def hand_has_basic(self):
+        '''
+        '''
+        mulligan = 0
+
+        while True:
+            if not self.draw_hand():
+                for card in self.bench:
+                    self.deck.append(card)
+
+                self.hand.clear()
+                self.shuffle_deck()
+                mulligan += 1
             else:
                 break
 
+        return True, mulligan
+    
+    def play_basic_to_active(self, pokemon: PokemonCard):
+        '''
+        '''
+        if self.active is not None:
+            return False
+        
+        if pokemon.stage != 0:
+            return False
+        
+        self.active = pokemon
+        self.hand.remove(pokemon)
+
+        return True
+    
+    def play_basic_to_bench(self, pokemon: PokemonCard, max_bench: int = 5):
+        '''
+        '''
+        if len(self.bench) >= max_bench:
+            return False
+        
+        if pokemon.stage != 0:
+            return False
+        
+        self.bench.append(pokemon)
+        self.hand.remove(pokemon)
+
+        return True
+    
     def set_prizes(self):
         '''
         '''
+        if self.prizes:
+            return False
+        
         for _ in range(6):
             card = self.deck.pop(0)
             self.prizes.append(card)
 
-    def take_prize_cards(self, amount = 1):
+        return True
+    
+
+    # Action Phase
+    def draw(self, amount: int = 1):
         '''
         '''
+        if not self.deck:
+            return False
+        
         for _ in range(amount):
-            if self.prizes:
-                card = self.pop(0)
-                self.hand.append(card)
+            card = self.deck.pop(0)
+            self.hand.append(card)
+
+    
+
+
+
+
+
+
+
+
